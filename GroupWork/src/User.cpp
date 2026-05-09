0dc5fc154030ec55f292b8f3e937924576fe5912
User <User@WINDOWS-5GCBQFC>
2026-05-09 13:59:58 +0900

    first

---
+++ GroupWork/src/User.cpp
@@ -0,0 +1,14 @@
+#include <board.h>
+
+void setup() {
+}
+
+void loop() {
+    while (1) {
+
+        digitalWrite(PIN_LED1, HIGH);
+        delay(1000);
+        digitalWrite(PIN_LED1, LOW);
+        delay(1000);
+    }
+}
