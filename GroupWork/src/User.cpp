dec6d8b1703fe2736df883cf34c747a0935028b6
urataku55 <takuto.uramoto.kensyu@gmail.com>
2026-05-12 13:32:27 +0900

    角度変化による速度調整追加 10~11:削除後、9~11追加 66~67:間に68~71を追加 102~115：削除後、107~139を追加

--- GroupWork/src/User.cpp
+++ GroupWork/src/User.cpp
@@ -1,4 +1,3 @@
-//コメント　湯浅
 #include <board.h>
 /***宣言***/
 /**define**/
@@ -7,8 +6,9 @@
 #define LONG_PRESS_TIME 3000			//長押し判定時間 ms秒
 #define BTN_ON HIGH						//ボタンON
 #define BTN_OFF LOW						//ボタンOFF
-#define MOTOR_SPEED 50					//通常モーター速度
-#define MAX_SPEED 55					//最大速度
+#define MOTOR_SPEEDL 51					//通常左モーター速度
+#define MOTOR_SPEEDR 50					//通常右モーター速度
+#define MAX_SPEED 63						//最大速度
 #define STOP_LINE 1000					//停止する閾値(白)
 /**enum**/
 typedef enum {
@@ -45,6 +45,7 @@ typedef struct {
 BtnState btnS;
 /**グローバル変数**/
 unsigned long now = 0;		//現在時刻
+
 /**プロトタイプ宣言**/
 void checkBtn(BtnState* pBtnS, State* pState);
 void runTimeMeasurement(RunState* pRunS, State* pState);
@@ -64,6 +65,10 @@ void setup() {
 void loop() {
 	/*現在時刻の取得*/
 	now = millis();
+	int baseSpeedL;              //左ベーススピード
+	int baseSpeedR;              //右ベーススピード
+	int diff;                   //急激な角度変化量に対応
+	static int prevGap = 0;                //直前の角度のgap
 	/**ボタンをチェックする(BTN_PERIOD ms秒)**/
 	if(now - btnS.prev >= BTN_PERIOD) {
 		btnS.prev = now;
@@ -99,19 +104,38 @@ void loop() {
 			/*ラインに戻ったらリセット*/
 			runS.outFlag = false;
 		}
+
 		/*センサー誤差確認*/
-		runS.gap = runS.sensorL - runS.sensorR;
+		//左に重みを少し持たせている
+		runS.gap = runS.sensorL+85 - runS.sensorR;//トレースセンサの誤差調整必要
+		diff = runS.gap -prevGap;
+
+		/*gapに応じて速度を連続変化*/
+		//基本速度に直接影響
+		baseSpeedL = MOTOR_SPEEDL - abs(runS.gap/60) - abs(diff/15);//80,20は調整必要
+		baseSpeedR = MOTOR_SPEEDR - abs(runS.gap/60) - abs(diff/15);
+
+		/*baseSpeedを上限下限値に調整*/
+		baseSpeedL = constrain(baseSpeedL,15,MOTOR_SPEEDL);
+		baseSpeedR = constrain(baseSpeedR,15,MOTOR_SPEEDR);
+
 		/*P制御*/
-		runS.control = runS.gap / 45;
+		//曲がるときの左右の車輪の回転数に直接影響
+		runS.control = runS.gap / 25 + diff / 7;//40,15も調整必要
+
 		/*モーター速度調整*/
-		runS.leftSpeed = MOTOR_SPEED - runS.control;
-		runS.rightSpeed = MOTOR_SPEED + runS.control;
-		/*速度制限*/
+		runS.leftSpeed = baseSpeedL - runS.control;
+		runS.rightSpeed = baseSpeedR + runS.control;
+
+		//速度制限
 		runS.leftSpeed = constrain(runS.leftSpeed,0,MAX_SPEED);
 		runS.rightSpeed = constrain(runS.rightSpeed,0,MAX_SPEED);
+
 		/*モーター始動*/
 		analogWrite(PIN_MOTOR_L, runS.leftSpeed);
 		analogWrite(PIN_MOTOR_R, runS.rightSpeed);
+
+		prevGap = runS.gap;
 		break;
 	/*停止状態*/
 	case STATE_STOP :
